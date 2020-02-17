34458b8af702a2f01761f1592f226ae205ad01d9
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-02-17 15:13:29 -0500

    Implemented endpoints in fileHandler.js

--- src/fileHandler.js
+++ src/fileHandler.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 const engine = fs.readFileSync(`${__dirname}/../client/engine.html`);
-const engine = fs.readFileSync(`${__dirname}/../client/engine.html`);
+const clientJs = fs.readFileSync(`${__dirname}/../client/js/index.js`);
+const threeJs = fs.readFileSync(`${__dirname}/../client/js/three.js`);
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
@@ -15,13 +16,20 @@ const getEngine = (request, response) => {
     response.end();
   };  
 const getIndexJs = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(engine);
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.write(clientJs);
+    response.end();
+};  
+const getThreeJs = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.write(threeJs);
     response.end();
 };  
 
 
 module.exports = {
   getIndex,
-  getEngine
+  getEngine,
+  getIndexJs,
+  getThreeJs
 };
