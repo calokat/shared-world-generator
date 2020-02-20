28ad1c4451e539a762f3fe07ee46bc02f40a14be
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-02-20 08:19:39 -0500

    Added TransformControls.js. Added get method in fileHandler

--- src/fileHandler.js
+++ src/fileHandler.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 const engine = fs.readFileSync(`${__dirname}/../client/engine.html`);
 const clientJs = fs.readFileSync(`${__dirname}/../client/js/index.js`);
-const threeJs = fs.readFileSync(`${__dirname}/../client/js/three.js`);
+const threeJs = fs.readFileSync(`${__dirname}/../client/js/three.module.js`);
+const engineJs = fs.readFileSync(`${__dirname}/../client/js/engine.js`);
+const transformControlsJs = fs.readFileSync(`${__dirname}/../client/js/TransformControls.js`);
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
@@ -11,25 +13,38 @@ const getIndex = (request, response) => {
 };
 
 const getEngine = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(engine);
-    response.end();
-  };  
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(engine);
+  response.end();
+};
 const getIndexJs = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/javascript' });
-    response.write(clientJs);
-    response.end();
-};  
+  response.writeHead(200, { 'Content-Type': 'application/javascript' });
+  response.write(clientJs);
+  response.end();
+};
 const getThreeJs = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/javascript' });
-    response.write(threeJs);
-    response.end();
-};  
+  response.writeHead(200, { 'Content-Type': 'application/javascript' });
+  response.write(threeJs);
+  response.end();
+};
+const getEngineJs = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/javascript' });
+  response.write(engineJs);
+  response.end();
+};
+const getTransformControlsJs = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/javascript' });
+  response.write(transformControlsJs);
+  response.end();
+};
+
 
 
 module.exports = {
   getIndex,
   getEngine,
   getIndexJs,
-  getThreeJs
+  getThreeJs,
+  getEngineJs,
+  getTransformControlsJs
 };
