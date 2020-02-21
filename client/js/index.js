5305dc5bca5bd380c87ca0926f70899d3d8a4d45
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-02-21 10:52:20 -0500

    Can now enter an existing id from the home page

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,7 @@
 let newWorldBtn = document.querySelector('#newWorld');
-
+let enterWorldBtn = document.querySelector("#enterWorld");
+let idField = document.querySelector("#idField");
+let errorDisplay = document.querySelector("#error");
 window.onload = () => {
     newWorldBtn.addEventListener('click', () => {
         let xhr = new XMLHttpRequest();
@@ -9,5 +11,20 @@ window.onload = () => {
             window.location.replace(window.location.href + `engine?id=${xhr.responseText}`);
         }
         xhr.send();
-    })
+    });
+    enterWorldBtn.addEventListener('click', () => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `/getScene?id=${idField.value}`);
+        xhr.onload = () => {
+            let jsonResponse = JSON.parse(xhr.response);
+            if (!jsonResponse.errorCode) {
+                window.location.replace(window.location.href += `engine?id=${idField.value}`);
+            }
+            // Thanks to https://www.w3schools.com/js/tryit.asp?filename=tryjs_visibility for a quick refresher
+            else {
+                errorDisplay.style.visibility = "visible";
+            }
+        }
+        xhr.send();
+    });
 }
