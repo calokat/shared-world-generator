95531ebf1677b7e0a17af167ebe49ec2e7ab05fb
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-03-02 10:13:51 -0500

    Commented. Replaced var with let

--- client/js/index.js
+++ client/js/index.js
@@ -1,13 +1,16 @@
+// https://github.com/mrdoob/three.js/blob/dev/build/three.module.js
 import * as THREE from '/three.module.js';
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// set up the scene, camera, and renderer
+// From https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.y = 5;
 camera.position.z = 5;
 camera.rotation.set(-Math.PI / 4, 0, 0, 'XYZ');
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+// prevent the right click menu from showing up on the canvas
 renderer.domElement.addEventListener('contextmenu', (e) => {
     e.preventDefault();
 })
@@ -20,9 +23,9 @@ animate();
 
 
 // create grid, with help from https://threejs.org/docs/index.html#api/en/helpers/GridHelper
-var size = 10;
-var divisions = 10;
-var gridHelper = new THREE.GridHelper( size, divisions );
+let size = 10;
+let divisions = 10;
+let gridHelper = new THREE.GridHelper( size, divisions );
 scene.add( gridHelper );
 
 
@@ -31,6 +34,7 @@ let enterWorldBtn = document.querySelector("#enterWorld");
 let idField = document.querySelector("#idField");
 let errorDisplay = document.querySelector("#error");
 window.onload = () => {
+    // gets a new id, then redirects to the app with a url containing the id
     newWorldBtn.addEventListener('click', () => {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', '/new');
@@ -45,6 +49,8 @@ window.onload = () => {
         }
         xhr.send();
     });
+    // takes the id text field input, checks if the id exists on the server,
+    // and if so puts it in the url to redirect.
     enterWorldBtn.addEventListener('click', () => {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', `/getScene?id=${idField.value}`);
@@ -52,7 +58,6 @@ window.onload = () => {
         xhr.onload = () => {
             let jsonResponse = JSON.parse(xhr.response);
             if (!jsonResponse.errorCode) {
-                // window.location.replace(window.location.href += `engine?id=${idField.value}`);
                 window.location.href = window.location.href += `engine?id=${idField.value}`;
             }
             // Thanks to https://www.w3schools.com/js/tryit.asp?filename=tryjs_visibility for a quick refresher
