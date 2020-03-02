aed1a101f5a16950a23c704f7244baf0512f6a20
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-03-02 08:24:58 -0500

    Commented and removed commented code

--- src/fileHandler.js
+++ src/fileHandler.js
@@ -1,52 +1,34 @@
 const fs = require('fs');
-
+// read all of the files
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 const engine = fs.readFileSync(`${__dirname}/../client/engine.html`);
 const clientJs = fs.readFileSync(`${__dirname}/../client/js/index.js`);
 const threeJs = fs.readFileSync(`${__dirname}/../client/js/three.module.js`);
 const engineJs = fs.readFileSync(`${__dirname}/../client/js/engine.js`);
 const transformControlsJs = fs.readFileSync(`${__dirname}/../client/js/TransformControls.js`);
-
+// generic helper method
 const writeResponse = (request, response, file, contentType) => {
   response.writeHead(200, { 'Content-Type': contentType });
   response.write(file);
   response.end();
 };
 const getIndex = (request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'text/html' });
-  // response.write(index);
-  // response.end();
   writeResponse(request, response, index, 'text/html');
 };
 
 const getEngine = (request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'text/html' });
-  // response.write(engine);
-  // response.end();
   writeResponse(request, response, engine, 'text/html');
 };
 const getIndexJs = (request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'application/javascript' });
-  // response.write(clientJs);
-  // response.end();
   writeResponse(request, response, clientJs, 'application/javascript');
 };
 const getThreeJs = (request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'application/javascript' });
-  // response.write(threeJs);
-  // response.end();
   writeResponse(request, response, threeJs, 'application/javascript');
 };
 const getEngineJs = (request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'application/javascript' });
-  // response.write(engineJs);
-  // response.end();
   writeResponse(request, response, engineJs, 'application/javascript');
 };
 const getTransformControlsJs = (request, response) => {
-  // response.writeHead(200, { 'Content-Type': 'application/javascript' });
-  // response.write(transformControlsJs);
-  // response.end();
   writeResponse(request, response, transformControlsJs, 'application/javascript');
 };
 
