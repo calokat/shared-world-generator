1d444fb2b560de77189edde40c3b95611ae4d865
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-03-02 10:16:09 -0500

    Ran npm test with autofixes

--- src/sceneHandler.js
+++ src/sceneHandler.js
@@ -4,26 +4,26 @@ const queryString = require('querystring');
 const scenes = {};
 
 const writeResponse = (request, response, status, contentType, message = '') => {
-  response.writeHead(status, {'Content-Type': contentType});
-  if (request.method !== "HEAD") {
+  response.writeHead(status, { 'Content-Type': contentType });
+  if (request.method !== 'HEAD') {
     response.write(message);
   }
   response.end();
-}
+};
 
 // sends a new id with shortid
 const getNewId = (request, response) => {
   if (request.headers.accept !== 'text/plain') {
-    writeResponse(request, response, 400, "text/plain", "Bad Request");
+    writeResponse(request, response, 400, 'text/plain', 'Bad Request');
     return;
   }
-  writeResponse(request, response, 200, "text/plain", shortid.generate());
+  writeResponse(request, response, 200, 'text/plain', shortid.generate());
 };
 // gets a scene from the scenes object by using the id param
 // in the query as a key
 const getScene = (request, response) => {
   if (request.headers.accept !== 'application/json') {
-    writeResponse(request, response, 400, "application/json", '{"message": "Bad Request"}');
+    writeResponse(request, response, 400, 'application/json', '{"message": "Bad Request"}');
     return;
   }
   const query = request.url.split('?')[1];
@@ -31,9 +31,9 @@ const getScene = (request, response) => {
   const sceneId = params.id;
   const scene = scenes[sceneId];
   if (scene) {
-    writeResponse(request, response, 200, "application/json", scene);
+    writeResponse(request, response, 200, 'application/json', scene);
   } else {
-    writeResponse(request, response, 404, "application/json", '{"errorCode": "Scene not found"}');
+    writeResponse(request, response, 404, 'application/json', '{"errorCode": "Scene not found"}');
   }
 };
 
@@ -48,23 +48,23 @@ const addOrUpdateScene = (request, response) => {
     const id = pairs[0].split('=')[1];
     // if they do not have a scene, it is a bad request
     if (!pairs[1]) {
-      writeResponse(request, response, 400, "application/json", "{'message': 'Bad Request'}");
+      writeResponse(request, response, 400, 'application/json', "{'message': 'Bad Request'}");
       return;
     }
     // get the second parameter
     const scene = pairs[1].split('=')[1];
-    let statusCode, message;
+    let statusCode; 
+    let message;
     // if the scene exists, send 204. If not, send 201
     if (scenes[id]) {
       statusCode = 204;
-      message = "Updated";
-    }
-    else {
+      message = 'Updated';
+    } else {
       statusCode = 201;
-      message = "Created";
+      message = 'Created';
     }
     scenes[id] = scene;
-    writeResponse(request, response, statusCode, "application/json", `{"message": "${message}"}`);
+    writeResponse(request, response, statusCode, 'application/json', `{"message": "${message}"}`);
   });
 };
 
